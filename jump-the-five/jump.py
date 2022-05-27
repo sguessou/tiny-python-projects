3bf9a10069bce8b2bd71f1aff001880f2e7efc3b
Saad Guessous <guessous_saad@hotmail.com>
2022-05-27 23:16:21 +0300

    Add jump the five exercise

---
+++ jump-the-five/jump.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Author : sguessou <sguessou@localhost>
+Date   : 2022-05-27
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="Jump the Five",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("text", metavar="str", help="Input text")
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    jumper = {
+        "1": "9",
+        "2": "8",
+        "3": "7",
+        "4": "6",
+        "5": "0",
+        "6": "4",
+        "7": "3",
+        "8": "2",
+        "9": "1",
+        "0": "5",
+    }
+    args = get_args()
+    new_text = ""
+    for char in args.text:
+        new_text += jumper.get(char, char)
+    print(new_text)
+
+
+# --------------------------------------------------
+if __name__ == "__main__":
+    main()
